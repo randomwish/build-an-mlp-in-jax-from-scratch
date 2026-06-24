0681b8467ba7fb2e29b53f9e605c9a9cd2174a0d
Yenzhe Wee <hanzhe100100@gmail.com>
2026-06-25 00:56:41 +0800

    Step 3: sample_normal_matrix

--- model.py
+++ model.py
@@ -26,8 +26,13 @@ def split_prng_key(key, num):
 
     # as noted, each PRNGKey is an array of shape (2,) so 2 rows with indet. number of col
 
-# Step 3 - sample_normal_matrix (not yet solved)
-# TODO: implement
+# Step 3 - sample_normal_matrix
+import jax
+import jax.numpy as jnp
+
+def sample_normal_matrix(key, shape):
+    # TODO: return a jnp array of the given shape with i.i.d. N(0,1) samples drawn from key
+    return jax.random.normal(key, shape = shape)
 
 # Step 4 - sample_input_features (not yet solved)
 # TODO: implement
